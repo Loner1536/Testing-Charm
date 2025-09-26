4112f0344783fa76c32f6d8f43d5c50058b35590
Loner1536 <woojuiscute1@gmail.com>
2025-09-26 08:56:09 +0000

    fixed rojo serve and removed waveclass

--- src/server/services/waveManager/class.ts
+++
@@ -1,6 +0,0 @@
-export default class WaveClass {
-    constructor() {
-        print("WaveManager initialized.");
-    }
-}
-
--- src/server/services/waveManager/index.ts
+++ src/server/services/waveManager/index.ts
@@ -1,12 +1,9 @@
 // Packages
 import { OnStart, Service } from "@flamework/core";
 
-// Classes
-import WaveClass from "./class";
-
 @Service()
 export class DataStore implements OnStart {
 	private startVotes: number = 0;
 
-    onStart(): void {}
+	onStart(): void {}
 }
